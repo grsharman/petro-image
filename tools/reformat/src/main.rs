2c53db596d772b9dbb08b41c2351dc816ad3adb0
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-10-13 19:28:44 -0400

    Add JSON reformatting tool

---
+++ tools/reformat/src/main.rs
@@ -0,0 +1,125 @@
+use std::{
+	env,
+	fs::File,
+	io::{BufReader, BufWriter},
+	process::Command,
+};
+
+use serde_json::{Map, Value, from_reader, json};
+
+fn main() {
+	// Read input and output paths.
+	let args: Vec<String> = env::args().collect();
+	let in_path = &args[1];
+	let out_path = &args[2];
+
+	// Read the existing samples file.
+	let in_file = File::open(in_path).expect("input file must be opened");
+	let reader = BufReader::new(in_file);
+	let samples: Value =
+		from_reader(reader).expect("input must be deserialized from JSON");
+
+	// Reformat the samples.
+	let value = reformat_samples(samples);
+
+	// Write the new file.
+	let out_file = File::create(out_path).expect("output file must be created");
+	let writer = BufWriter::new(out_file);
+	serde_json::to_writer_pretty(writer, &value)
+		.expect("output must be serialized to JSON");
+
+	// Format with Prettier.
+	Command::new("prettier.cmd")
+		.args(vec!["-w", out_path])
+		.output()
+		.expect("output must be formatted with Prettier");
+}
+
+fn reformat_samples(samples: Value) -> Value {
+	let Value::Object(samples) = samples else {
+		panic!("samples must be an object");
+	};
+
+	// While reformatting samples, move the sample names from outer object keys
+	// to fields within the sample objects themselves.
+	let samples: Value = samples.into_values().map(reformat_sample).collect();
+
+	// Tuck the existing outer object under "samples" to leave space for
+	// metadata in the future.
+	json!({"samples": samples})
+}
+
+fn reformat_sample(sample: Value) -> Value {
+	let Value::Object(mut sample) = sample else {
+		panic!("sample must be an object");
+	};
+
+	// Move struct-of-array style fields into individual sample objects.
+	let Some(Value::Array(labels)) = sample.get("tileLabels").cloned() else {
+		panic!("sample must contain tile labels array");
+	};
+	let Value::Array(angle_sets) = sample
+		.get("tileSetRotationAngles")
+		.unwrap_or(&Value::Array(Vec::new()))
+		.clone()
+	else {
+		panic!("tile set rotation angles must be an array");
+	};
+	let Value::Array(periods) = sample
+		.get("tileSetRotationAngleRepeat")
+		.unwrap_or(&Value::Array(Vec::new()))
+		.clone()
+	else {
+		panic!("tile set rotation angle repeat must be an array");
+	};
+
+	let Some(Value::Array(tile_sets)) = sample.remove("tileSets") else {
+		panic!();
+	};
+	let tile_sets = tile_sets
+		.into_iter()
+		.enumerate()
+		.map(|(i, tile_set)| {
+			// Tile sets are currently allowed to be either a URI or an array of
+			// URIs. Convert them uniformly to an array of objects, each of
+			// which has explicit an URI and (optionally) angle.
+			let mut images: Vec<Map<String, Value>> = match tile_set {
+				uri @ Value::String(..) => {
+					vec![Map::from_iter([("uri".into(), uri)])]
+				}
+				Value::Array(uris) => uris
+					.into_iter()
+					.map(|uri| Map::from_iter([("uri".into(), uri)]))
+					.collect(),
+				_ => panic!("unexpected tile set type at index {i}"),
+			};
+			if let Some(angles) = angle_sets.get(i) {
+				for (j, image) in images.iter_mut().enumerate() {
+					if let Some(angle) = angles.get(j)
+						&& angle.is_number()
+					{
+						image.insert("angleDegrees".into(), angle.clone());
+					}
+				}
+			}
+
+			let mut tile_set =
+				Map::from_iter([("label".into(), labels[i].clone())]);
+			if let Some(period @ Value::Number(..)) = periods.get(i) {
+				tile_set.insert("periodDegrees".into(), period.clone());
+			}
+			tile_set.insert("tiles".into(), images.into());
+			tile_set
+		})
+		.collect();
+	sample.insert("tileSets".into(), tile_sets);
+
+	// Remove migrated and unused fields.
+	sample.shift_remove("tileLabels");
+	sample.shift_remove("tileSetRotationAngles");
+	sample.shift_remove("tileSetRotationSpacing");
+	sample.shift_remove("tileSetRotationAngleRepeat");
+	sample.shift_remove("tileSetRotation");
+
+	Value::Object(sample)
+}
