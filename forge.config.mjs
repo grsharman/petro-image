a1ba6ef2c9364ad7419a5b1e3913a016f17c8d05
uacsrg <uacsrg@gmail.com>
2026-01-10 15:28:27 -0600

    Changes allowing Windows installer build

--- forge.config.mjs
+++ forge.config.mjs
@@ -11,62 +11,35 @@ export default {
   },
   rebuildConfig: {},
   makers: [
-    // Installer for Windows using NSIS (in progress)
     // {
-    //   name: "@electron-forge/maker-nsis",
-    //   platforms: ["win32"],
+    //   name: "@electron-forge/maker-zip",
+    //   platforms: ["darwin"],
     //   config: {
-    //     // Installer metadata
-    //     name: "petro-image", // The name of your app
-    //     authors: "Sharman, G.R., Sharman, J.P.", // Appears in installer metadata
-    //     exe: "petro-image.exe", // Name of the generated executable
-    //     description: "petro-image app for Windows",
-
-    //     // Icons
-    //     setupIcon: "assets/icon.ico", // Icon for the installer
-    //     icon: "assets/icon.ico", // Icon for the installed app and shortcuts
-
-    //     // Installer behavior
-    //     oneClick: false, // false = user sees "Next/Back" pages
-    //     perMachine: false, // false = installs for current user only
-    //     allowElevation: true, // Allow admin permissions if needed
-    //     allowToChangeInstallationDirectory: true, // Let user choose install folder
-    //     runAfterFinish: true, // Run app after install finishes
-
-    //     // Shortcuts
-    //     shortcutName: "petro-image", // Name of Start Menu / desktop shortcuts
-    //     shortcutDir: "Desktop", // Can be "Desktop", "StartMenu", or both
+    //     name: `petro-image-${version}`,
     //   },
     // },
-    {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-      config: {
-        name: `petro-image-${version}`,
-      },
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      platforms: ["linux"],
-      config: {
-        options: {
-          icon: "assets/icon.png",
-          categories: ["Utility", "Education"],
-          packageName: `petro-image-${version}`,
-        },
-      },
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      platforms: ["linux"],
-      config: {
-        options: {
-          icon: "assets/icon.png",
-          categories: ["Utility", "Education"],
-          packageName: `petro-image-${version}`,
-        },
-      },
-    },
+    // {
+    //   name: "@electron-forge/maker-deb",
+    //   platforms: ["linux"],
+    //   config: {
+    //     options: {
+    //       icon: "assets/icon.png",
+    //       categories: ["Utility", "Education"],
+    //       packageName: `petro-image-${version}`,
+    //     },
+    //   },
+    // },
+    // {
+    //   name: "@electron-forge/maker-rpm",
+    //   platforms: ["linux"],
+    //   config: {
+    //     options: {
+    //       icon: "assets/icon.png",
+    //       categories: ["Utility", "Education"],
+    //       packageName: `petro-image-${version}`,
+    //     },
+    //   },
+    // },
   ],
   plugins: [
     {
